387133bc2d4f396a6b7479ea75e87b3fc908d7d9
Leonid Umanskiy <splatt@splatt.ru>
2017-05-02 11:31:01 -0500

    Minor fix in the QueryParserService

--- src/services/queryparser/index.js
+++ src/services/queryparser/index.js
@@ -9,14 +9,16 @@ function QueryParserService() {
     var self = this;
 
     this.fromQueryOrBody = function fromQueryOrBody(req, key){
-        if (req.query[key]) {
+        if (req.query && req.query[key]) {
             try {
                 return JSON.parse(req.query[key]);
             } catch (e) {
                 return next(new InvalidQueryError("query is not a valid json"));
             }
-        } else {
+        } else if(req.body && req.body[key]) {
             return req.body[key];
+        } else {
+            return null;
         }
     }
 }
